50dd162ecf2bf46034747405386fa0440bad8375
TeddyFrye <tfrye1227@gmail.com>
2023-10-18 11:54:06 -0500

    Removes testing files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,17 @@
 import { useState } from "react";
 import "../styles/App.css";
-import Greeting from "./Greeting.jsx";
-import Animals from "./Animals.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Greeting />
+      <App />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          {count} click, ah ah ah.
+          {count} click, ah ah ah!
         </button>
       </div>
-      <Animals />
     </>
   );
 }
